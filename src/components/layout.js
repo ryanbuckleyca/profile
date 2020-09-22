bc3b6f30f75002cc95b12f4a4135ff8ff61ef4e9
Ryan Buckley <ryanbuckley@gmail.com>
2020-09-22 17:38:11 -0400

    better space for xl screen

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,17 @@
 import React from "react"
+import { Helmet } from 'react-helmet'
 import "./layout.css"
 
 export default function Layout({ children }) {
-  return <div>{children}</div>
+  return <>
+    <Helmet>
+      <title>Ryan Buckley • Web Developer and Photo Archives Specialist</title>
+      <meta property="og:url" content="//ryanbuckley.ca" />
+      <meta property="og:type" content="" />
+      <meta property="og:title" content="Ryan Buckley" />
+      <meta property="og:description" content="Web Developer and Photo Archives Specialist" />
+      <meta property="og:image" content="//ryanbuckley.ca/images/screenshot.png" />
+    </Helmet>
+    {children}
+  </>
 }
