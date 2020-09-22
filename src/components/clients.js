eecec789546ebde29298fe5bbe5ca8d137390806
Ryan Buckley <ryanbuckley@gmail.com>
2020-09-22 17:00:30 -0400

    move data to json file

--- src/components/clients.js
+++ src/components/clients.js
@@ -1,68 +1,8 @@
 import React from "react"
 import "./clients.css"
+import data from "./data.json"
 
-const clientList = [
-  {
-    name: 'Susan Meiselas',
-    href: 'http://www.susanmeiselas.com/',
-    avatar: 'mes.jpg',
-  },
-  {
-    href: 'https://www.imogencunningham.com/',
-    avatar: 'cui.jpg',
-    name: 'Imogen Cunningham'
-  },
-  {
-    href: 'https://www.elliotterwitt.com/',
-    avatar: 'ere.jpg',
-    name: 'Elliott Erwitt'
-  },
-  {
-    href: 'https://www.magnumphotos.com/photographer/paul-fusco/',
-    avatar: 'fup.jpg',
-    name: 'Paul Fusco'
-  },
-  {
-    href: 'http://www.ytobarrada.com/',
-    avatar: 'yto.jpg',
-    name: 'Yto Barrada'
-  },
-  {
-    href: 'https://wendyewald.com/',
-    avatar: 'eww.jpg',
-    name: 'Wendy Ewald'
-  },
-  {
-    href: 'http://robertburley.com/',
-    avatar: 'bur.jpg',
-    name: 'Robert Burley'
-  },
-  {
-    href: 'https://www.abigailheyman.com/',
-    avatar: 'hea.jpg',
-    name: 'Abigail Heyman'
-  },
-  {
-    href: 'http://meghannriepenhoff.com/',
-    avatar: 'rim.jpg',
-    name: 'Meghann Riepenhoff'
-  },
-  {
-    href: 'https://www.legacy.com/obituaries/nytimes/obituary.aspx?n=dorothy-m-beskind&pid=173026644',
-    avatar: 'bed.jpg',
-    name: 'Dorothy Beskind'
-  },
-  {
-    href: 'https://www.henrygrossman.com/',
-    avatar: 'grh.jpg',
-    name: 'Henry Grossman'
-  },
-  {
-    href: 'https://www.kenheyman.com/index',
-    avatar: 'hek.jpg',
-    name: 'Ken Heyman'
-  }
-]
+const clientList = data.artists
 
 export default function clients() {
   return(
