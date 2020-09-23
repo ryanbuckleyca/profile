0595e43cb243ad735de8a338e50fbea385cd237e
Ryan Buckley <ryanbuckley@gmail.com>
2020-09-23 17:40:40 -0400

    move data.json to index, remove layout, and add props

--- src/components/organizations.js
+++ src/components/organizations.js
@@ -1,15 +1,12 @@
 import React from "react"
 import "./organizations.css"
-import data from "./data.json"
 
-const orgList = data.orgs
-
-export default function organizations() {
+export default function organizations(props) {
   return(
     <div id="org-list">
       <h2>Organizations/Institutions:</h2>
       {
-        orgList.map(org =>
+        props.data.map(org =>
           <a href={org.href}><img alt={org.alt} src={'logos/' + org.logo} /></a>
         )
       }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,14 +1,11 @@
 import React from "react"
 import "./projects.css"
-import data from "./data.json"
 
-const projectList = data.projects
-
-export default function projects() {
+export default function projects(props) {
   return (
     <div id='app-list' className='row'>
     {
-      projectList.map(project =>
+      props.data.map(project =>
       <a href={project.href} title=''>
         <div className='card-img' style={{backgroundImage: `url('apps/${project.img}')`}}></div>
         <div className='card-title' style={{backgroundImage: `url('images/wave${Math.floor(Math.random()*5)+1}.svg')`}}>
