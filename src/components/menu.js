b84340377eb7eb42fcf6bd86a1c04f8cf0c062dc
Ryan Buckley <ryanbuckley@gmail.com>
2020-10-02 00:52:49 -0400

    account for gatbsy not rendering window on build

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,20 +1,23 @@
-import React from "react"
+import React, {Component} from "react"
 import "./menu.css"
 
-export default function menu() {
-  return (
-    <div id="menu">
-      <a href="#clients">
-        <img src="images/heart.png" className="icon heart" />
-        clients
-      </a>
-      <a href="#apps"><img src="images/tool.png" className="icon rocket" />
-        { window.innerWidth < 475 ? "apps" : "applications" }
-      </a>
-      <a href="#contact">
-        <img src="images/send.png" className="icon wave" />
-        contact
-      </a>
-    </div>
-  )
+class Menu extends Component {
+  render(){
+    return (
+      <div id="menu">
+        <a href="#clients">
+          <img src="images/heart.png" className="icon heart" />
+          clients
+        </a>
+        <a href="#apps"><img src="images/tool.png" className="icon rocket" />
+          { this.props.windowWidth < 475 ? "apps" : "applications" }
+        </a>
+        <a href="#contact">
+          <img src="images/send.png" className="icon wave" />
+          contact
+        </a>
+      </div>
+    )
+  }
 }
+export default Menu;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {Component} from "react"
 import { Helmet } from 'react-helmet'
 import "../components/index.css";
 import Menu from "../components/menu";
@@ -8,53 +8,66 @@ import Clients from "../components/clients";
 import Organizations from "../components/organizations";
 import data from "../components/data.json"
 
-export default function Home() {
+class Home extends Component {
 
-  React.useEffect(()=>{
-    document.addEventListener('scroll', (e) => {
-      console.log('scrolling');
-    });
-  })
+  constructor() {
+    super()
+    this.state = {windowWidth: window.innerWidth};
+  }
 
-  return <>
-     <Helmet>
-        <title>Ryan Buckley • Web Developer and Photo Archives Specialist</title>
-        <meta property="og:url" content="//ryanbuckley.ca" />
-        <meta property="og:type" content="" />
-        <meta property="og:title" content="Ryan Buckley" />
-        <meta property="og:description" content="Web Developer and Photo Archives Specialist" />
-        <meta property="og:image" content="//ryanbuckley.ca/images/screenshot.png" />
-    </Helmet>
-    <div id="container">
-      <div id="top-section">&nbsp;</div>
-      <main id="content">
-        <Header />
-        <Menu />
-        <div className="entry-content">
-          <a className="anchor" name="clients" href="#">&nbsp;</a>
-          <h1>clients</h1>
-          <Organizations data={data.orgs} />
-          <Clients data={data.artists} />
-        </div>
-        <div className="entry-content">
-          <a className="anchor" name="apps" href="#">&nbsp;</a>
-          <h1>applications</h1>
-          <Projects data={data.projects} />
-        </div>
-        <div className="entry-content">
-          <a className="anchor" name="contact" href="#">&nbsp;</a>
-          <h1>contact</h1>
-          For now, the best way to reach me is through one of the following:
-          <h2 className="contact-icons">
-              <a href="https://www.linkedin.com/in/ryanbuckleyca/" aria-label="linkedIn"><img src={'/images/socialicons_01.png'} alt="LinkedIn icon" /></a>
-              <a href="https://www.instagram.com/apinrise/"aria-label="instagram"><img src={'images/socialicons_03.png'} alt="Instagram icon" /></a>
-              <a href="https://facebook.com/ryanbuckleyca"aria-label="facebook"><img src={'images/socialicons_04.png'} alt="Facebook icon" /></a>
-          </h2>
-        </div>
-      </main>
-    </div>
-    <footer>
-      <div>&copy; copyright 2019, all rights reserved</div>
-    </footer>
-  </>
+  updateDimensions = () => {
+    this.setState({ windowWidth: window.innerWidth});
+  };
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  render() {
+    return <>
+       <Helmet>
+          <title>Ryan Buckley • Web Developer and Photo Archives Specialist</title>
+          <meta property="og:url" content="//ryanbuckley.ca" />
+          <meta property="og:type" content="" />
+          <meta property="og:title" content="Ryan Buckley" />
+          <meta property="og:description" content="Web Developer and Photo Archives Specialist" />
+          <meta property="og:image" content="//ryanbuckley.ca/images/screenshot.png" />
+      </Helmet>
+      <div id="container">
+        <div id="top-section">&nbsp;</div>
+        <main id="content">
+          <Header />
+          <Menu windowWidth={this.state.windowWidth} />
+          <div className="entry-content">
+            <a className="anchor" name="clients" href="#">&nbsp;</a>
+            <h1>clients</h1>
+            <Organizations data={data.orgs} />
+            <Clients data={data.artists} />
+          </div>
+          <div className="entry-content">
+            <a className="anchor" name="apps" href="#">&nbsp;</a>
+            <h1>applications</h1>
+            <Projects data={data.projects} />
+          </div>
+          <div className="entry-content">
+            <a className="anchor" name="contact" href="#">&nbsp;</a>
+            <h1>contact</h1>
+            For now, the best way to reach me is through one of the following:
+            <h2 className="contact-icons">
+                <a href="https://www.linkedin.com/in/ryanbuckleyca/" aria-label="linkedIn"><img src={'/images/socialicons_01.png'} alt="LinkedIn icon" /></a>
+                <a href="https://www.instagram.com/apinrise/"aria-label="instagram"><img src={'images/socialicons_03.png'} alt="Instagram icon" /></a>
+                <a href="https://facebook.com/ryanbuckleyca"aria-label="facebook"><img src={'images/socialicons_04.png'} alt="Facebook icon" /></a>
+            </h2>
+          </div>
+        </main>
+      </div>
+      <footer>
+        <div>&copy; copyright 2019, all rights reserved</div>
+      </footer>
+    </>
+  }
 }
+
+export default Home;
